d6e425258e25d663d08633b5442d0084d61affe6
jime shen <stjdydayou@163.com>
2014-01-02 03:43:29 -0800

    初始导入源码

---
+++ ctrl/MainCtrl.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * JPrass framework
+ * @copyright  Copyright (c) 2013 www.jprass.com
+ * @filename	MainCtrl.class.php
+ * @author		jprass.com
+ * @mail		i@jprass.com
+ * @QQ			97142822
+ * @version    1.0
+ */
+
+class MainCtrl extends BaseCtrl {
+
+	public function index() {
+		return "main";
+	}
+}
+
+?>
